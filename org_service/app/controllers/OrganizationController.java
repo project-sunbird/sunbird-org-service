536a4b86b0cbd334c3b706cff32b9bd6ef5892ac
Karthik Kumarasamy <kkarthik4cs@gmail.com>
2018-06-19 16:02:34 +0530

    Issue #SB-4221  feat: Org micro service refactoring

--- org_service/app/controllers/OrganizationController.java
+++ org_service/app/controllers/OrganizationController.java
@@ -1,6 +1,8 @@
 package controllers;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.sunbird.common.models.util.ActorOperations;
@@ -8,7 +10,7 @@ import org.sunbird.common.models.util.JsonKey;
 import org.sunbird.common.models.util.LoggerEnum;
 import org.sunbird.common.models.util.ProjectLogger;
 import org.sunbird.common.models.util.ProjectUtil;
-import org.sunbird.common.request.ExecutionContext;
+import org.sunbird.common.models.util.ProjectUtil.EsType;
 import org.sunbird.common.request.Request;
 import org.sunbird.common.request.RequestValidator;
 import org.sunbird.controllers.BaseController;
@@ -36,23 +38,10 @@ public class OrganizationController extends BaseController {
 			JsonNode requestData = request().body().asJson();
 			ProjectLogger.log("OrganisationController: createOrg called" + requestData, LoggerEnum.DEBUG.name());
 			Request reqObj = createAndInitRequest(ActorOperations.CREATE_ORG.getValue(), requestData);
-
-			HashMap<String, Object> innerMap = new HashMap<>();
-			Map<String, Object> address = (Map<String, Object>) reqObj.getRequest().get(JsonKey.ADDRESS);
 			Map<String, Object> orgData = reqObj.getRequest();
-			if (null != address && address.size() > 0 && orgData.containsKey(JsonKey.ADDRESS)) {
-				innerMap.put(JsonKey.ADDRESS, address);
-				orgData.remove(JsonKey.ADDRESS);
-			}
-			if (orgData.containsKey(JsonKey.PROVIDER)) {
-				orgData.put(JsonKey.PROVIDER, ((String) orgData.get(JsonKey.PROVIDER)).toLowerCase());
-			}
-			if (orgData.containsKey(JsonKey.EXTERNAL_ID)) {
-				orgData.put(JsonKey.EXTERNAL_ID, ((String) orgData.get(JsonKey.EXTERNAL_ID)).toLowerCase());
-			}
-			innerMap.put(JsonKey.ORGANISATION, orgData);
-			innerMap.put(JsonKey.REQUESTED_BY, ctx().flash().get(JsonKey.USER_ID));
-			reqObj.setRequest(innerMap);
+			ProjectUtil.updateMapValueToLowerCase(orgData, JsonKey.PROVIDER, JsonKey.EXTERNAL_ID);
+			Map<String, Object> requestMap = createRequestMap(orgData, true);
+			reqObj.setRequest(requestMap);
 			return actorResponseHandler(getActorRef(), reqObj, timeout, null, request());
 		} catch (Exception e) {
 			return Promise.<Result>pure(createCommonExceptionResponse(e, request(), true));
@@ -68,24 +57,91 @@ public class OrganizationController extends BaseController {
 	public Promise<Result> updateOrg() {
 
 		try {
-			
 			JsonNode requestData = request().body().asJson();
 			ProjectLogger.log("OrganisationController: updateOrg called with data = " + requestData,
 					LoggerEnum.DEBUG.name());
 			Request reqObj = createAndInitRequest(ActorOperations.UPDATE_ORG.getValue(), requestData);
-			HashMap<String, Object> innerMap = new HashMap<>();
-			Map<String, Object> address = (Map<String, Object>) reqObj.getRequest().get(JsonKey.ADDRESS);
 			Map<String, Object> orgData = reqObj.getRequest();
-			if (null != address && address.size() > 0 && orgData.containsKey(JsonKey.ADDRESS)) {
-				innerMap.put(JsonKey.ADDRESS, address);
-				orgData.remove(JsonKey.ADDRESS);
-			}
-			innerMap.put(JsonKey.ORGANISATION, orgData);
-			innerMap.put(JsonKey.REQUESTED_BY, ctx().flash().get(JsonKey.USER_ID));
-			reqObj.setRequest(innerMap);
+			Map<String, Object> requestMap = createRequestMap(orgData, true);
+			reqObj.setRequest(requestMap);
 			return actorResponseHandler(getActorRef(), reqObj, timeout, null, request());
 		} catch (Exception e) {
 			return Promise.<Result>pure(createCommonExceptionResponse(e, request(), true));
 		}
+	}
+
+	/**
+	 * This method will provide user profile details based on requested userId.
+	 *
+	 * @return Promise<Result>
+	 */
+	public Promise<Result> getOrgDetails() {
+
+		try {
+			JsonNode requestData = request().body().asJson();
+			ProjectLogger.log("OrganisationController: getOrgDetails called with data = " + requestData,
+					LoggerEnum.DEBUG.name());
+			Request reqObj = createAndInitRequest(ActorOperations.GET_ORG_DETAILS.getValue(), requestData);
+			ProjectUtil.updateMapSomeValueTOLowerCase(reqObj);
+			RequestValidator.validateOrg(reqObj);
+			Map<String, Object> orgData = reqObj.getRequest();
+			Map<String, Object> requestMap = createRequestMap(orgData, false);
+			reqObj.setRequest(requestMap);
+			return actorResponseHandler(getActorRef(), reqObj, timeout, null, request());
+		} catch (Exception e) {
+			return Promise.<Result>pure(createCommonExceptionResponse(e, request(), true));
+		}
+	}
+
+	/**
+	 * This method will do the organisation search for Elastic search. this will
+	 * internally call composite search api.
+	 *
+	 * @return Promise<Result>
+	 */
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public Promise<Result> search() {
+		try {
+			JsonNode requestData = request().body().asJson();
+			ProjectLogger.log("OrganisationController: search called", LoggerEnum.DEBUG.name());
+			Request reqObj = createAndInitRequest(ActorOperations.COMPOSITE_SEARCH.getValue(), requestData);
+			ProjectUtil.updateMapSomeValueTOLowerCase(reqObj);
+			List<String> esObjectType = new ArrayList<>();
+			esObjectType.add(EsType.organisation.getTypeName());
+
+			if (reqObj.getRequest() != null && reqObj.getRequest().containsKey(JsonKey.FILTERS)
+					&& reqObj.getRequest().get(JsonKey.FILTERS) != null
+					&& reqObj.getRequest().get(JsonKey.FILTERS) instanceof Map) {
+				((Map) (reqObj.getRequest().get(JsonKey.FILTERS))).put(JsonKey.OBJECT_TYPE, esObjectType);
+			} else {
+				Map<String, Object> dataMap = new HashMap<>();
+				dataMap.put(JsonKey.OBJECT_TYPE, esObjectType);
+				if (reqObj.getRequest() != null) {
+					reqObj.getRequest().put(JsonKey.FILTERS, dataMap);
+				} else {
+					Map<String, Object> requestmap = new HashMap<>();
+					requestmap.put(JsonKey.FILTERS, dataMap);
+					reqObj.setRequest(requestmap);
+				}
+			}
+			return actorResponseHandler(getActorRef(), reqObj, timeout, null, request());
+		} catch (Exception e) {
+			return Promise.<Result>pure(createCommonExceptionResponse(e, request(), true));
+		}
+	}
+
+	private Map<String, Object> createRequestMap(Map<String, Object> orgData, boolean updateAddress) {
+		Map<String, Object> requestMap = new HashMap<>();
+		if (updateAddress) {
+			Map<String, Object> address = (Map<String, Object>) orgData.get(JsonKey.ADDRESS);
+			if (null != address && address.size() > 0 && orgData.containsKey(JsonKey.ADDRESS)) {
+				requestMap.put(JsonKey.ADDRESS, address);
+				orgData.remove(JsonKey.ADDRESS);
+			}
+		}
+		requestMap.put(JsonKey.ORGANISATION, orgData);
+		requestMap.put(JsonKey.REQUESTED_BY, ctx().flash().get(JsonKey.USER_ID));
+
+		return requestMap;
 	}
 }
