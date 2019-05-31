341f3c1d2e040e311719c8b7d6947f9405f9d13c
Mayank Morya <moryamayank9@gmail.com>
2019-05-31 14:54:18 +0530

    logs added and routes changed to v3 from v1

--- org-service/app/controllers/organisationmanagement/OrgMemberController.java
+++ org-service/app/controllers/organisationmanagement/OrgMemberController.java
@@ -1,5 +1,8 @@
 package controllers.organisationmanagement;
 
+import org.sunbird.common.models.util.LoggerEnum;
+import org.sunbird.common.models.util.ProjectLogger;
+
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Results;
@@ -7,12 +10,12 @@ import play.mvc.Results;
 public class OrgMemberController extends Controller {
 
   public Result addMemberToOrganisation() {
-  //  ProjectLogger.log("Start add member to Org ",LoggerEnum.INFO);
+    ProjectLogger.log("Start add member to Org ",LoggerEnum.INFO);
     return Results.ok("200");
   }
 
   public Result removeMemberFromOrganisation() {
-  //  ProjectLogger.log("Start remove member from Org",LoggerEnum.INFO);
+    ProjectLogger.log("Start remove member from Org",LoggerEnum.INFO);
     return Results.ok("200");
   }
 }
--- org-service/app/controllers/organisationmanagement/OrgTypeController.java
+++ org-service/app/controllers/organisationmanagement/OrgTypeController.java
@@ -1,5 +1,8 @@
 package controllers.organisationmanagement;
 
+import org.sunbird.common.models.util.LoggerEnum;
+import org.sunbird.common.models.util.ProjectLogger;
+
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Results;
@@ -7,17 +10,17 @@ import play.mvc.Results;
 public class OrgTypeController extends Controller {
 
   public Result createOrgType() {
- //   ProjectLogger.log("Start Org type Creation Contoller",LoggerEnum.INFO);
+    ProjectLogger.log("Start Org type Creation Contoller",LoggerEnum.INFO);
     return Results.ok("200");
   }
 
   public Result updateOrgType() {
- //   ProjectLogger.log("Start Org update Contoller",LoggerEnum.INFO);
+    ProjectLogger.log("Start Org update Contoller",LoggerEnum.INFO);
     return Results.ok("200");
   }
 
   public Result listOrgType() {
- //   ProjectLogger.log("Start Org list Contoller",LoggerEnum.INFO);
+    ProjectLogger.log("Start Org list Contoller",LoggerEnum.INFO);
     return Results.ok("200");
   }
 }
